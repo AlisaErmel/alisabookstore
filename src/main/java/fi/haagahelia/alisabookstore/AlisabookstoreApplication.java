719f6d66546a323f040451e6f8436f01a6319fee
Alisa Ermel <alisa.ermel@gmail.com>
2025-08-26 08:59:07 +0300

    First commit

---
+++ src/main/java/fi/haagahelia/alisabookstore/AlisabookstoreApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.alisabookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlisabookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlisabookstoreApplication.class, args);
+	}
+
+}
---
+++ src/main/java/fi/haagahelia/alisabookstore/web/BookController.java
@@ -0,0 +1,5 @@
+package fi.haagahelia.alisabookstore.web;
+
+public class BookController {
+
+}
