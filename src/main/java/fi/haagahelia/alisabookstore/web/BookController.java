b997993133b94802bfcbde283509ecbb9ad9efe2
Alisa Ermel <alisa.ermel@gmail.com>
2025-09-08 17:38:31 +0300

    Add Listpage 3.2

--- src/main/java/fi/haagahelia/alisabookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/alisabookstore/web/BookController.java
@@ -1,12 +1,20 @@
 package fi.haagahelia.alisabookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import fi.haagahelia.alisabookstore.model.BookRepository;
 
+@Controller
 public class BookController {
-
+    @Autowired
     private BookRepository repository;
 
-    public BookController(BookRepository repository) {
-        this.repository = repository;
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 }
