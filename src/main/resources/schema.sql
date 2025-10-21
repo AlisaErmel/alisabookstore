780da4eafc7e1748ad93780a011a2298b056d8a3
Alisa Ermel <alisa.ermel@gmail.com>
2025-10-21 09:56:57 +0300

    Delete unnecessary file

--- src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS book (
-    book_id int(5) NOT NULL AUTO_INCREMENT,
-    title varchar(50) DEFAULT NULL,
-    author varchar(50) DEFAULT NULL,
-    publication_year int(5) DEFAULT NULL,
-    isbn varchar(50) DEFAULT NULL,
-    price double() DEFAULT NULL,
-    PRIMARY KEY(student_id)
-    );
